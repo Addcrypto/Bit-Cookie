3013a5cec4b13ebac95f2801252676e1c42694a7
Cuuann <nicholasannguyen@gmail.com>
2023-10-19 14:50:46 -0500

    Settings Menu and Credits Scene Added
    mostly skeleton for the menu still need to work on pause menu functions.

--- Main.gd
+++ Main.gd
@@ -8,3 +8,16 @@ func _on_quitButton_pressed():
 
 func _on_newGameButton_pressed():
   get_tree().change_scene("res://dummyImport.tscn")
+
+
+func _on_settingsButton_pressed():
+    get_tree().change_scene("res://optionsMenu.tscn")
+
+
+
+func _on_backButton_pressed():
+    get_tree().change_scene("res://mainMenu.tscn")
+
+
+func _on_creditsButton_pressed():
+    get_tree().change_scene("res://creditsScene.tscn")
