14f4e8df38603ca27f9d7cd529cda94cc6439760
PabloSan19 <pablo.sanchez13@outlook.com>
2023-11-30 18:05:42 -0600

    Fixed Flying Enemy LVL2

--- src/scripts/Coin.gd
+++ src/scripts/Coin.gd
@@ -15,7 +15,7 @@ func _ready():
 #func _process(delta):
 #  pass
 
-func _on_Coin_body_entered(body):
+func _on_Coin_body_entered(_body):
   print( "Player grabbed a coin." )
 #  if("Player") in body.name:
 #    PlayerVariables.Coins += 1;
